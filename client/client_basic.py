37f814f19e109042e7a00d2459a3fbd5a195c57a
Hirmay Sandesara <56473003+Hirmay@users.noreply.github.com>
2022-05-08 12:47:39 +0530

    Add files via upload
    Also includes initiation of client functions

---
+++ client/client_basic.py
@@ -0,0 +1,121 @@
+import socket
+import threading, wave, pyaudio, time, queue
+import sys
+import struct
+import numpy as np
+#Wait for incoming data from server
+#.decode is used to turn the message in bytes to a string
+def receive(sock, signal, multi_list, stat_int):
+    while signal:
+        # try:
+            # data = socket.recv(32)
+            # print(str(data.decode("utf-8")))
+        multi_station = multi_list[stat_int]
+        print(multi_station)
+        BUFF_SIZE = 65536 #this will later have to be calculated, t (taken as input) * bitrate provided by server
+        client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        p = pyaudio.PyAudio()
+        CHUNK = 1024*10
+        MCAST_GRP = '224.1.1.1'
+        MCAST_PORT = 5007
+        client_socket.bind(('', MCAST_PORT))
+        group = socket.inet_aton(MCAST_GRP)
+        mreq = struct.pack('4sl', group, socket.INADDR_ANY)
+        client_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,1)
+        input_client()
+        stream = p.open(format=p.get_format_from_width(2),
+                        channels=2,
+                        rate=44100,
+                        output=True,
+                        frames_per_buffer=CHUNK)
+        # create socket
+       
+        # message = b'Hello'
+        # client_socket.sendto(message,("localhost",5445))
+        DATA_SIZE,_= client_socket.recvfrom(BUFF_SIZE)
+        # print("Hello")
+        # DATA_SIZE = int(DATA_SIZE.decode())
+        # while True:
+       
+        q = queue.Queue(maxsize=10000000)
+        cnt=0
+        print("Hello")
+        def getAudioData():
+            while True:
+                frame,_= client_socket.recvfrom(BUFF_SIZE)
+                q.put(frame)
+                # print('[Queue size while loading]...',q.qsize())
+                    
+        t1 = threading.Thread(target=getAudioData, args=())
+        t1.start()
+        time.sleep(3)
+        # DURATION = DATA_SIZE*CHUNK/44100
+        print('[Now Playing]... Data','[Audio Time]:','seconds')
+        while True:
+            frame = q.get()
+            stream.write(frame)
+            # time.sleep(0.0001)
+            # print('[Queue size while playing]...',q.qsize(),'[Time remaining...]',round(DURATION),'seconds')
+            # DURATION-=CHUNK/44100
+            if(q.qsize()==0):
+                break
+        client_socket.close()
+        print('Audio closed')
+        # break
+
+
+        # except:
+        #     print("You have been disconnected from the server")
+        #     signal = False
+        #     break
+
+#Get host and port
+host = "localhost"
+port = 5432
+
+#Attempt connection to server
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    multi_msg = sock.recv(1024).decode()
+    print(multi_msg)
+    # this will convert string into an ordered list
+    multi_list = eval(multi_msg)
+    print(multi_list)
+    station = input("Enter the station you want to listen to: ")
+    # the stat_int is the index of the station in the list, which will be sent to recieve
+    # so as to know which station to listen to
+    # and further would be used for switch station feature
+    stat_int = int(station)
+except:
+    print("Could not make a connection to the server")
+    input("Press enter to quit")
+    sys.exit(0)
+# this will get the station info from the selected choice    
+
+sock.send(station.encode())
+
+""" We need to get the signal to either pause, restart, terminate
+or switch the respective station
+It will be necessary to have a different thread which can do this for us   """
+m_port = 5007
+def input_client():
+    while True:
+        client_msg = "Type P, R, T or S at any point you want to Pause, Restart, Terminate, or Switch respectively: "
+        message = input(client_msg)
+        client_socket.sendto(message,("localhost",5007))
+        # do something with the message
+#Create new thread to wait for data
+# other args will be given based on the selection
+receiveThread = threading.Thread(target = receive, args = (sock, True, multi_list, stat_int))
+receiveThread.start()
+#inputThread = threading.Thread(target=input_client)
+#inputThread.start()
+
+#Send data to server
+#str.encode is used to turn the string message into bytes so it can be sent across the network
+# while True:
+#     message = input()
+#     sock.sendall(str.encode(message))
---
+++ server/server_basic.py
@@ -0,0 +1,135 @@
+import socket
+import threading, wave, pyaudio, time
+import math
+import struct
+import numpy as np
+
+#Variables for holding information about connections
+connections = []
+total_connections = 0
+
+#Client class, new instance created for each connected client
+#Each instance has the socket and address that is associated with items
+#Along with an assigned ID and a name chosen by the client
+class Client(threading.Thread):
+    def __init__(self, socket, address, id, name, signal):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.id = id
+        self.name = name
+        self.signal = signal
+    
+    def __str__(self):
+        return str(self.id) + " " + str(self.address)
+    
+    #Attempt to get data from client
+    #If unable to, assume client has disconnected and remove him from server data
+    #If able to and we get data back, print it in the server and send it back to every
+    #client aside from the client that has sent it
+    #.decode is used to convert the byte data into a printable string
+    def run(self):
+
+        while self.signal:
+            try:
+                data = self.socket.recv(32)
+            except:
+                print("Client " + str(self.address) + " has disconnected")
+                self.signal = False
+                connections.remove(self)
+                break
+            if data != "":
+                print("ID " + str(self.id) + ": " + str(data.decode("utf-8")))
+
+                # for client in connections:
+                #     if client.id != self.id:
+                #         client.socket.sendall(data)
+# This will be a static message containing station information in an ordered list being sent to client.       
+multi_msg = [[0, 'Blinding Lights', 'This station will play blinding lights', '127.0.0.1', 5007, 'info_port-dont know', 44100],
+[1, 'Blinding Lights', 'This station will play blinding lights', '127.0.0.1', 5007, 'info_port-dont know', 44100],
+[2, 'Blinding Lights', 'This station will play blinding lights', '127.0.0.1', 5007, 'info_port-dont know', 44100]]
+# this will convert the list to a string which then will be sent to the client as a encoded message
+multi_str = str(multi_msg)
+#Wait for new connections
+def newConnections(socket):
+    while True:
+        sock, address = socket.accept()
+        global total_connections
+        connections.append(Client(sock, address, total_connections, "Name", True))
+        connections[-1].start()
+        print("New connection at ID " + str(connections[-1]))
+        total_connections += 1
+        sock.send(multi_str.encode())
+        station = sock.recv(1024).decode()
+        print(station)
+
+
+# class Station1(threading.Thread):
+#     def __init__(self):
+#         threading.Thread.__init__(self)
+
+def station1():
+    BUFF_SIZE = 65536
+    MCAST_GRP = '127.0.0.1'
+    MCAST_PORT = 5007
+    MULTICAST_TTL = 2
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, BUFF_SIZE)
+
+    while(True):
+        print("Hello")
+        CHUNK = 1024*10
+        wf = wave.open("./../songs/lights.wav")
+        p = pyaudio.PyAudio()
+        # print('server listening at',("localhost", 5445),wf.getframerate())
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        input=True,
+                        frames_per_buffer=CHUNK)
+
+        data = None
+        sample_rate = wf.getframerate()
+        
+        # msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+        # print('[GOT connection from]... ',client_addr,msg)
+        # DATA_SIZE = math.ceil(wf.getnframes()/CHUNK)
+        # DATA_SIZE = str(DATA_SIZE).encode()
+        # print('[Sending data size]...',wf.getnframes()/sample_rate)
+        # server_socket.sendto(DATA_SIZE,client_addr)
+        # while True:
+        
+        cnt=0
+        while True:
+            
+            data = wf.readframes(CHUNK)
+            server_socket.sendto(data,(MCAST_GRP, MCAST_PORT))
+            time.sleep(0.001) # Here you can adjust it according to how fast you want to send data keep it > 0
+            # print(cnt)
+            if cnt >(wf.getnframes()/CHUNK):
+                break
+            cnt+=1
+
+            # break
+        print('SENT...')            
+
+def main():
+    #Get host and port
+    host = "localhost"
+    port = 5432
+
+    #Create new server socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(5)
+
+    #Create new thread to wait for connections
+    newConnectionsThread = threading.Thread(target = newConnections, args = (sock,))
+    newConnectionsThread.start()
+
+    station1Thread = threading.Thread(target=station1)
+    station1Thread.start()
+    
+    
+main()
